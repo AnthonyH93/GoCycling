ec5ec36bdc900d19f4b5b42c32b4371abf7c00c3
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2021-03-14 18:51:01 -0400

    Initial Commit

---
+++ Go Cycling/Go_CyclingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Go_CyclingApp.swift
+//  Go Cycling
+//
+//  Created by Anthony Hopkins on 2021-03-14.
+//
+
+import SwiftUI
+
+@main
+struct Go_CyclingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
