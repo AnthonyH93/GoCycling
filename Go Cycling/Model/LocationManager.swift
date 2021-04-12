1d66d104841e156ab0e38334f72381bd57c1459c
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2021-04-11 20:27:51 -0400

    Adding state object for location manager

---
+++ Go Cycling/Model/LocationManager.swift
@@ -0,0 +1,52 @@
+//
+//  LocationManager.swift
+//  Go Cycling
+//
+//  Created by Anthony Hopkins on 2021-04-10.
+//
+
+import Foundation
+import CoreLocation
+import Combine
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+
+    private let locationManager = CLLocationManager()
+    @Published var locationStatus: CLAuthorizationStatus?
+    @Published var lastLocation: CLLocation?
+
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    var statusString: String {
+        guard let status = locationStatus else {
+            return "unknown"
+        }
+        
+        switch status {
+        case .notDetermined: return "notDetermined"
+        case .authorizedWhenInUse: return "authorizedWhenInUse"
+        case .authorizedAlways: return "authorizedAlways"
+        case .restricted: return "restricted"
+        case .denied: return "denied"
+        default: return "unknown"
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        locationStatus = status
+        print(#function, statusString)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        lastLocation = location
+        print(#function, location)
+    }
+}
--- Go Cycling/View/MapView.swift
+++ Go Cycling/View/MapView.swift
@@ -11,20 +11,16 @@ import CoreLocation
 
 struct MapView: UIViewRepresentable {
   typealias UIViewType = MKMapView
-  
-//  func makeUIView(context: Context) -> MKMapView {
-//    let mapView = MKMapView()
-//
-//    let region = MKCoordinateRegion(
-//      center: CLLocationCoordinate2D(latitude: 40.71, longitude: -74),
-//      span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
-//    mapView.setRegion(region, animated: true)
-//
-//    return mapView
-//  }
-//
-//  func updateUIView(_ uiView: MKMapView, context: Context) {}
-    let locationManager = CLLocationManager()
+
+    @StateObject var locationManager = LocationManager()
+    
+    var userLatitude: String {
+        return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
+    }
+        
+    var userLongitude: String {
+        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
+    }
 
     func makeUIView(context: Context) -> MKMapView {
         MKMapView(frame: .zero)
@@ -32,37 +28,14 @@ struct MapView: UIViewRepresentable {
     
     func updateUIView(_ view: MKMapView, context: Context) {
         view.showsUserLocation = true
-        if locationManager.authorizationStatus == .notDetermined {
-            locationManager.requestWhenInUseAuthorization()
-        }
         
-        if locationManager.authorizationStatus != .authorizedAlways {
-            locationManager.requestAlwaysAuthorization()
-        }
         
-//        locationManager.requestAlwaysAuthorization()
-//        locationManager.requestWhenInUseAuthorization()
+        let authStatus = locationManager.statusString
         
-        var hasPermission = false
-        switch locationManager.authorizationStatus {
-            case .authorizedAlways, .authorizedWhenInUse:
-                hasPermission = true
-            default:
-                hasPermission = false
-        }
-        
-        if hasPermission {
-            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-            locationManager.startUpdatingLocation()
-            let location: CLLocationCoordinate2D = locationManager.location!.coordinate
+        if (authStatus == "authorizedAlways" || authStatus == "authorizedWhenInUse") {
+            let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(CLLocationDegrees(userLatitude)!, CLLocationDegrees(userLongitude)!)
             let span = MKCoordinateSpan(latitudeDelta: 0.009, longitudeDelta: 0.009)
             let region = MKCoordinateRegion(center: location, span: span)
-            view.setRegion(region, animated: true)
-        }
-        else {
-            let region = MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 40.71, longitude: -74),
-            span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
             view.setRegion(region, animated: true)
         }
     }
