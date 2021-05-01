645cf6464ae8218c629f1ead9a3b00f14a3945c2
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2021-05-01 19:44:32 -0400

    Added user preferences extension for retrieving stored sort choice

---
+++ Go Cycling/Models/Core Data/UserPreferences/UserPreferences+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  UserPreferences+Extensions.swift
+//  Go Cycling
+//
+//  Created by Anthony Hopkins on 2021-05-01.
+//
+
+import Foundation
+import CoreData
+
+extension UserPreferences {
+    static func storedSortingChoice() -> SortChoice {
+        let context = PersistenceController.shared.container.viewContext
+        let fetchRequest: NSFetchRequest<UserPreferences> = UserPreferences.fetchRequest()
+        do {
+            let items = try context.fetch(fetchRequest)
+            return items[0].sortingChoiceConverted
+        }
+        catch let error as NSError {
+            print("Error getting UserPreferences: \(error.localizedDescription), \(error.userInfo)")
+        }
+        return .dateDescending
+    }
+}
