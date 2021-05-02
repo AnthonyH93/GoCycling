cfeb29634b04ea038d38ada407ec08be5e73fbf2
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2021-05-01 23:22:34 -0400

    Fixing large metrics view padding

--- Go Cycling/Views/Cycle/LargeMetricsView.swift
+++ Go Cycling/Views/Cycle/LargeMetricsView.swift
@@ -21,60 +21,57 @@ struct LargeMetricsView: View {
     
     var body: some View {
         HStack {
-            Spacer()
-            ZStack {
-                if (preferences.storedPreferences[0].displayingMetrics) {
-                    HStack {
-                        ZStack {
-                            Rectangle()
-                                .fill(Color(UserPreferences.convertColourChoiceToUIColor(colour: preferences.storedPreferences[0].colourChoiceConverted)))
-                                .opacity(0.4)
-                                .frame(width: screenWidth/3, height: 90)
-                                .cornerRadius(10)
-                            HStack {
-                                Spacer()
-                                LargeSingleMetricView(metricName: "Distance",
-                                                      metricText: MetricsFormatting.formatDistanceWithoutUnits(distance: self.setCurrentDistance(),
-                                                      usingMetric: preferences.storedPreferences[0].usingMetric),
-                                                      metricUnits: MetricsFormatting.getDistanceUnits(usingMetric: preferences.storedPreferences[0].usingMetric))
-                                Spacer()
-                            }
-                        }
-                        ZStack {
-                            Rectangle()
-                                .fill(Color(UserPreferences.convertColourChoiceToUIColor(colour: preferences.storedPreferences[0].colourChoiceConverted)))
-                                .opacity(0.4)
-                                .frame(width: screenWidth/3, height: 90)
-                                .cornerRadius(10)
-                            HStack {
-                                Spacer()
-                                LargeSingleMetricView( metricName: "Speed",
-                                                       metricText: MetricsFormatting.formatSpeedWithoutUnits(speed: currentSpeed ?? 0.0,
-                                                       usingMetric: preferences.storedPreferences[0].usingMetric),
-                                                       metricUnits: MetricsFormatting.getSpeedUnits(usingMetric: preferences.storedPreferences[0].usingMetric))
-                                Spacer()
-                            }
-                        }
-                        ZStack {
-                            Rectangle()
-                                .fill(Color(UserPreferences.convertColourChoiceToUIColor(colour: preferences.storedPreferences[0].colourChoiceConverted)))
-                                .opacity(0.4)
-                                .frame(width: screenWidth/3, height: 90)
-                                .cornerRadius(10)
-                            HStack {
-                                Spacer()
-                                LargeSingleMetricView(metricName: "Altitude",
-                                                      metricText: MetricsFormatting.formatElevationWithoutUnits(elevation: currentAltitude ?? 0.0,
-                                                      usingMetric: preferences.storedPreferences[0].usingMetric),
-                                                      metricUnits: MetricsFormatting.getElevationUnits(usingMetric: preferences.storedPreferences[0].usingMetric))
-                                Spacer()
-                            }
+            if (preferences.storedPreferences[0].displayingMetrics) {
+                HStack(alignment: .center, spacing: 10) {
+                    ZStack {
+                        Rectangle()
+                            .fill(Color(UserPreferences.convertColourChoiceToUIColor(colour: preferences.storedPreferences[0].colourChoiceConverted)))
+                            .opacity(0.4)
+                            .frame(width: screenWidth/3 - 10, height: 90)
+                            .cornerRadius(10)
+                        HStack {
+                            Spacer()
+                            LargeSingleMetricView(metricName: "Distance",
+                                                  metricText: MetricsFormatting.formatDistanceWithoutUnits(distance: self.setCurrentDistance(),
+                                                  usingMetric: preferences.storedPreferences[0].usingMetric),
+                                                  metricUnits: MetricsFormatting.getDistanceUnits(usingMetric: preferences.storedPreferences[0].usingMetric))
+                            Spacer()
                         }
                     }
-                    .padding(.top, 10)
+                    ZStack {
+                        Rectangle()
+                            .fill(Color(UserPreferences.convertColourChoiceToUIColor(colour: preferences.storedPreferences[0].colourChoiceConverted)))
+                            .opacity(0.4)
+                            .frame(width: screenWidth/3 - 10, height: 90)
+                            .cornerRadius(10)
+                        HStack {
+                            Spacer()
+                            LargeSingleMetricView( metricName: "Speed",
+                                                   metricText: MetricsFormatting.formatSpeedWithoutUnits(speed: currentSpeed ?? 0.0,
+                                                   usingMetric: preferences.storedPreferences[0].usingMetric),
+                                                   metricUnits: MetricsFormatting.getSpeedUnits(usingMetric: preferences.storedPreferences[0].usingMetric))
+                            Spacer()
+                        }
+                    }
+                    ZStack {
+                        Rectangle()
+                            .fill(Color(UserPreferences.convertColourChoiceToUIColor(colour: preferences.storedPreferences[0].colourChoiceConverted)))
+                            .opacity(0.4)
+                            .frame(width: screenWidth/3 - 10, height: 90)
+                            .cornerRadius(10)
+                        HStack {
+                            Spacer()
+                            LargeSingleMetricView(metricName: "Altitude",
+                                                  metricText: MetricsFormatting.formatElevationWithoutUnits(elevation: currentAltitude ?? 0.0,
+                                                  usingMetric: preferences.storedPreferences[0].usingMetric),
+                                                  metricUnits: MetricsFormatting.getElevationUnits(usingMetric: preferences.storedPreferences[0].usingMetric))
+                            Spacer()
+                        }
+                    }
                 }
             }
         }
+        .padding(.all, 10)
     }
     
     func setCurrentDistance() -> CLLocationDistance {
