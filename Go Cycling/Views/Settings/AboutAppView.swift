bc5bd5294bc792623114fe1091849fc82a7b7fe5
Anthony Hopkins <anthony5hopkins@outlook.com>
2021-08-16 00:19:44 -0400

    Adding open source link to settings tab

--- Go Cycling/Views/Settings/AboutAppView.swift
+++ Go Cycling/Views/Settings/AboutAppView.swift
@@ -10,14 +10,26 @@ import SwiftUI
 struct AboutAppView: View {
     
     let appVersionNumber = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+    let openSourceURL = NSURL(string: "https://github.com/AnthonyH93/GoCycling")! as URL
     let privacyPolicyURL = NSURL(string: "https://anthony55hopkins.wixsite.com/gocycling/privacy-policy")! as URL
     let termsAndConditionsURL = NSURL(string: "https://anthony55hopkins.wixsite.com/gocycling/terms-and-conditions")! as URL
+    
+    @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
         HStack {
             Text("App Version")
             Spacer()
             Text(appVersionNumber)
+        }
+        HStack {
+            Text("Go Cycling is Open Source")
+            Spacer()
+            Button(action: {
+                UIApplication.shared.open(openSourceURL, options: [:], completionHandler: nil)
+            }) {
+                Image(colorScheme == .dark ? "GitHub-Light" : "GitHub-Dark")
+            }
         }
         Link("View Privacy Policy", destination: privacyPolicyURL)
         Link("View Terms and Conditions", destination: termsAndConditionsURL)
