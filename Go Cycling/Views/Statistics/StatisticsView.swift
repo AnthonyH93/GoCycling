6042dbf2bce9c84daec17dd4b21864277fb4a6a6
Anthony Hopkins <anthony5hopkins@outlook.com>
2021-08-29 00:13:57 -0400

    Adding statistics tab

---
+++ Go Cycling/Views/Statistics/StatisticsView.swift
@@ -0,0 +1,20 @@
+//
+//  StatisticsView.swift
+//  Go Cycling
+//
+//  Created by Anthony Hopkins on 2021-08-23.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        Text("Statistics Tab")
+    }
+}
+
+struct StatisticsView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatisticsView()
+    }
+}
