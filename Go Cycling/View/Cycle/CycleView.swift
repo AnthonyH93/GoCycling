440f42f40cfac45d830b02a80e85ba2e420704e9
Anthony Hopkins <anthony5hopkins@outlook.com>
2024-06-28 00:07:52 -0400

    Pause the cycling session if the user decides to visit settings from location alert

--- Go Cycling/View/Cycle/CycleView.swift
+++ Go Cycling/View/Cycle/CycleView.swift
@@ -31,6 +31,8 @@ struct CycleView: View {
                     Alert(title: Text("Location settings may not be correct"),
                           message: Text(locationManager.locationSettingsAlertMessage),
                           primaryButton: .default(Text("Open Settings")) {
+                            // Pause the current cycling session
+                            self.timer.pause()
                             // Open Settings app
                             UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
                           },
