e32f4a6be22040a4820490c48a2722d1080d47af
Anthony Hopkins <anthony5hopkins@outlook.com>
2024-07-03 01:56:27 -0400

    First pass at adding a health kit setting

--- Go Cycling/View/Settings/SyncSettingsView.swift
+++ Go Cycling/View/Settings/SyncSettingsView.swift
@@ -18,6 +18,7 @@ struct SyncSettingsView: View {
     var body: some View {
         // In iOS 16+ forms can have labels attached to content
         if #available(iOS 16.0, *) {
+            // iCloud sync setting
             LabeledContent {
                 Toggle("", isOn: $preferences.iCloudOn)
                     .onChange(of: preferences.iCloudOn) { value in
@@ -35,6 +36,17 @@ struct SyncSettingsView: View {
                 Label("iCloud", systemImage: "icloud")
                 Text("Sync all data with iCloud")
             }
+            // HealthKit sync setting
+            LabeledContent {
+                Toggle("", isOn: $preferences.iCloudOn)
+                    .onChange(of: preferences.iCloudOn) { value in
+                        preferences.updateBoolPreference(preference: CustomizablePreferences.iCloudSync, value: value)
+                        self.showingAlert = true
+                    }
+            } label: {
+                Label("Health", systemImage: "heart")
+                Text("Upload data to the Health app")
+            }
         } else {
             Toggle("iCloud Sync", isOn: $preferences.iCloudOn)
                 .onChange(of: preferences.iCloudOn) { value in
@@ -47,6 +59,12 @@ struct SyncSettingsView: View {
                           message: Text("Please restart the application to \(preferences.iCloudOn ? "enable" : "disable") iCloud syncing for cycling routes.")
                             
                     )
+                }
+            // TODO: Make sure this works in iOS 15
+            Toggle("Health Sync", isOn: $preferences.iCloudOn)
+                .onChange(of: preferences.iCloudOn) { value in
+                    preferences.updateBoolPreference(preference: CustomizablePreferences.iCloudSync, value: value)
+                    self.showingAlert = true
                 }
         }
     }
